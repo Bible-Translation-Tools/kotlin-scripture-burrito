cd413b2ecc3d021c7549bcc6741c2d78ba7442e4
Joe <jsarabia1247@gmail.com>
2024-05-24 09:07:08 -0400

    Create TestMetadataSubtypes.kt

---
+++ src/test/kotlin/org/bibletranslationtools/scriptureburrito/TestMetadataSubtypes.kt
@@ -0,0 +1,159 @@
+package org.bibletranslationtools.scriptureburrito
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.bibletranslationtools.scriptureburrito.flavor.FlavorType
+import org.bibletranslationtools.scriptureburrito.flavor.scripture.audio.*
+import org.junit.Assert
+import org.junit.Test
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.*
+
+class TestMetadataSubtypes {
+
+    val copyright = CopyrightSchema().apply {
+        this.shortStatements = mutableListOf(ShortStatement("CC BY-SA", "en"))
+    }
+
+    val audioFlavor = TypeSchema(
+        FlavorType(Flavor.SCRIPTURE).apply {
+            val formats = Formats()
+            formats.put("format-wav", AudioFormat(Compression.WAV))
+            formats.put("format-mp3", AudioFormat(Compression.MP3))
+            this.flavor = AudioFlavorSchema(
+                mutableSetOf(Performance.READING, Performance.SINGLE_VOICE),
+                formats
+            ).apply {
+                name = "audioTranslation"
+            }
+        }
+    )
+
+    val enLanguage = Languages().apply {
+        add(
+            LanguageSchema("en")
+        )
+    }
+
+    val derivedAudio = DerivedMetadataSchema(
+        Format.SCRIPTURE_BURRITO,
+        DerivedMetaSchema(
+            dateCreated = Date.from(Instant.now()),
+            version = MetaVersionSchema("1.0.0"),
+            defaultLocale = "en",
+            generator = SoftwareAndUserInfoSchema().apply {
+                softwareName = "test"
+                softwareVersion = "1.0.0"
+            }
+        ),
+        IdAuthoritiesSchema(),
+        IdentificationSchema(),
+        confidential = false,
+        copyright = copyright,
+        type = audioFlavor,
+        languages = enLanguage
+    )
+
+    val sourceAudio = SourceMetadataSchema(
+        Format.SCRIPTURE_BURRITO,
+        SourceMetaSchema(
+            dateCreated = Date.from(Instant.now()),
+            version = MetaVersionSchema("1.0.0"),
+            defaultLocale = "en",
+            generator = SoftwareAndUserInfoSchema().apply {
+                softwareName = "test"
+                softwareVersion = "1.0.0"
+            }
+        ),
+        IdAuthoritiesSchema(),
+        IdentificationSchema(),
+        confidential = false,
+        copyright = copyright,
+        type = audioFlavor,
+        languages = enLanguage
+    )
+
+    val templateAudio = TemplateMetadataSchema(
+        Format.SCRIPTURE_BURRITO,
+        TemplateMetaSchema(
+            dateCreated = Date.from(Instant.now()),
+            version = MetaVersionSchema("1.0.0"),
+            defaultLocale = "en",
+            templateName = LocalizedText(short = hashMapOf("en" to "testTemplate")),
+            generator = SoftwareAndUserInfoSchema().apply {
+                softwareName = "test"
+                softwareVersion = "1.0.0"
+            }
+        ),
+        IdAuthoritiesSchema(),
+        IdentificationSchema(),
+        confidential = false,
+        copyright = copyright,
+        type = audioFlavor,
+        languages = enLanguage
+    )
+
+    val mapper = ObjectMapper()
+
+    init {
+        mapper.registerKotlinModule()
+        mapper.configure(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS, false)
+        mapper.setDateFormat(SimpleDateFormat("yyyy-MM-dd"))
+    }
+
+    @Test
+    fun testSerializesToSourceMetadata() {
+        val audio = mapper.writeValueAsString(sourceAudio)
+        val read = mapper.readValue(audio, SourceMetadataSchema::class.java)
+        assert(read is SourceMetadataSchema)
+    }
+
+    @Test
+    fun testSerializesToTemplateMetadata() {
+        val audio = mapper.writeValueAsString(templateAudio)
+        val read = mapper.readValue(audio, TemplateMetadataSchema::class.java)
+        assert(read is TemplateMetadataSchema)
+    }
+
+    @Test
+    fun testSerializesToDerivedMetadata() {
+        val audio = mapper.writeValueAsString(derivedAudio)
+        val read = mapper.readValue(audio, DerivedMetadataSchema::class.java)
+        assert(read is DerivedMetadataSchema)
+    }
+
+    @Test
+    fun testDeserializesToSourceMetadata() {
+        val audio = mapper.writeValueAsString(sourceAudio)
+        val read = mapper.readTree(audio)
+        assert(read["format"].asText() == Format.SCRIPTURE_BURRITO.value())
+        assert(read["meta"]["category"].asText() == "source")
+        assert(read["type"]["flavorType"]["name"].asText() == "scripture")
+        assert(read["type"]["flavorType"]["flavor"]["name"].asText() == "audioTranslation")
+        assert(read["languages"][0]["tag"].asText() == "en")
+    }
+
+    @Test
+    fun testDeserializesToTemplateMetadata() {
+        val audio = mapper.writeValueAsString(sourceAudio)
+        val read = mapper.readTree(audio)
+        assert(read["format"].asText() == Format.SCRIPTURE_BURRITO.value())
+        assert(read["meta"]["category"].asText() == "template")
+        assert(read["type"]["flavorType"]["name"].asText() == "scripture")
+        assert(read["type"]["flavorType"]["flavor"]["name"].asText() == "audioTranslation")
+        assert(read["languages"][0]["tag"].asText() == "en")
+    }
+
+    @Test
+    fun testDeserializesToDerivedMetadata() {
+        val audio = mapper.writeValueAsString(sourceAudio)
+        val read = mapper.readTree(audio)
+        assert(read["format"].asText() == Format.SCRIPTURE_BURRITO.value())
+        assert(read["meta"]["category"].asText() == "derived")
+        assert(read["type"]["flavorType"]["name"].asText() == "scripture")
+        assert(read["type"]["flavorType"]["flavor"]["name"].asText() == "audioTranslation")
+        assert(read["languages"][0]["tag"].asText() == "en")
+    }
+}
